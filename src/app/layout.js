6982e0ae7777f96ef9ddd6c1f76238f2fbdaf92a
Rohit Londhe <rohitlondhe446@gmail.com>
2023-06-04 13:02:17 +0530

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter, Roboto, Poppins } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { ThemeProvider } from "@/context/ThemeContext";
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Next Js 13 Blog Website",
+  description: "This is the Website Made using latest Next js 13.4 new features and using the mongodb as database",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider>
+          <AuthProvider>
+            <div className="container">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </AuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
