fd274bc198b463d1eb2d6c5926770140fd9eae66
Rohit Londhe <rohitlondhe446@gmail.com>
2023-06-06 12:00:03 +0530

    production fixes

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  // const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+  //   cache: "no-store",
+  // });
+
+  const res = await fetch(`${process.env.API_BASE_URL}/posts/${id}`, {
     cache: "no-store",
   });
 
@@ -57,7 +61,7 @@ const BlogPost = async ({ params }) => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-         {data.content}
+          {data.content}
         </p>
       </div>
     </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,10 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  // const res = await fetch("http://localhost:3000/api/posts", {
+  //   cache: "no-store",
+  // });
+  const res = await fetch(`${process.env.API_BASE_URL}/posts`, {
     cache: "no-store",
   });
 
